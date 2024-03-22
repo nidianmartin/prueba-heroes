4fb3fcb3d1b89193304546506df7c4aaa2c3349c
Nidian Martin <nidianmartin@gmail.com>
2024-03-22 12:46:07 +0100

    pipe-hero: se mejora el pipe y se agrega imagen general para nuevos heroes sin imagen

--- src/app/pipes/hero-image.pipe.ts
+++ src/app/pipes/hero-image.pipe.ts
@@ -7,8 +7,8 @@ import { Hero } from '../components/heroes/state';
 })
 export class HeroImagePipe implements PipeTransform {
   transform(hero: Hero): string {
-    if (!hero.id && !hero.alt_img) {
-      return 'assets/no-image.png';
+    if (!hero.id || !hero.alt_img) {
+      return 'assets/heroes/no-image.png';
     }
     if (hero.alt_img) {
       return hero.alt_img; //https://google.com/flash.png
--- src/environments/environments.prod.ts
+++
@@ -1,3 +0,0 @@
-// export const environment = {
-//   baseUrl: 'http://heroes-app.com/api',
-// };
